b887ca3bf60498601de89ec8e140c16303e9cdd8
Arsen <kuzmin.arsee@yandex.ru>
2020-03-12 13:01:15 +0300

    random theme is added

--- src/main/java/theme/ThemeColor.java
+++ src/main/java/theme/ThemeColor.java
@@ -1,14 +1,32 @@
 package theme;
 
+import java.awt.*;
+import java.util.Random;
+
 public class ThemeColor {
     private final long id;
     private final String fontColor;
     private final String backgroundColor;
 
+    private String getRandomColor(){
+        Random rand = new Random();
+        float r = rand.nextFloat();
+        float g = rand.nextFloat();
+        float b = rand.nextFloat();
+        Color color = new Color(r,g,b);
+        String hex = Integer.toHexString(color.getRGB() & 0xffffff);
+        if (hex.length() < 6) {
+            hex = "0" + hex;
+        }
+        hex = "#" + hex;
+        return hex;
+    }
     public ThemeColor(long id) {
+
+
         this.id = id;
-        this.fontColor = "#E8793E";
-        this.backgroundColor = "#3EC5E4";
+        this.fontColor  =getRandomColor();
+        this.backgroundColor = getRandomColor();
     }
 
     public String getFontColor() {
